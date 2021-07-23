674ea01373b3417efe86a50c896baa459f5aa4ba
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2021-07-23 17:21:59 +0800

    greetings for multiple people

--- create-a-Go-module/hello/hello.go
+++ create-a-Go-module/hello/hello.go
@@ -14,16 +14,23 @@ func main(){
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	//Request a greeting message.
-	message, err := greetings.Hello("Eleven")
+	// A slice of names.
+	names := []string{
+		"Gladys",
+		"Samantha",
+		"Darrin",
+	}
 
 	// If an error was returned, print it to the console and
     // exit the program.
+
+	messages, err := greetings.Hellos(names)
+
 	if err != nil{
 		log.Fatal(err)
 	}
 
 	// If no error was returned, print the returned message
     // to the console.
-	fmt.Println(message)
+	fmt.Println(messages)
 }
