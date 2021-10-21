4f73157c4d823372656143a1bab2499455032204
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2021-10-21 19:04:46 +0800

    update

--- GoBible/ch3/surface.go
+++ GoBible/ch3/surface.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"math"
-	"net/http"
 )
 
 const (
@@ -19,15 +17,8 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("localhost:8000",nil))
-	
-}
-
-func handler(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "image/svg+xml")
-	fmt.Fprintf(w,"<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: blue; stroke-width: 0.7' "+
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
@@ -35,11 +26,11 @@ func handler(w http.ResponseWriter, r *http.Request){
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Fprintf(w,"<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Fprintln(w,"</svg>")
+	fmt.Println("</svg>")
 }
 
 func corner(i, j int) (float64, float64) {
@@ -58,5 +49,5 @@ func corner(i, j int) (float64, float64) {
 
 func f(x, y float64) float64 {
 	r := math.Hypot(x, y) // distance from (0,0)
-	return math.Sin(r)/r;
+	return math.Sin(r) / r
 }
