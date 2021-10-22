8b73ed20a9810328a0eb07fe3b82dc1da32cdfd9
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2021-10-22 18:07:58 +0800

    update

---
+++ GoBible/ch5/linksmain/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"Learning-Golang/GoBible/ch5/links"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	urls := os.Args[1:]
+	for _, url := range urls {
+		links, err := links.Extract(url)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		for i, link := range links {
+			fmt.Printf("%3d   %s\n", i, link)
+		}
+	}
+}
